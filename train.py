3bafac1ec719dbe198fde0882e5c345d0044520b
Jitendra Pandey <jitendra@infinstor.com>
2022-09-29 22:12:23 -0700

    import sklearn

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import argparse
 
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
