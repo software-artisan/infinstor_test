6d23f43fa28458bd08f19bbb96fc476347fe377a
Jitendra Pandey <jitendra@infinstor.com>
2022-09-29 22:32:57 -0700

    Revert "xgboost autolog"
    This reverts commit 3b2c358b465d458d1c232d11f871e6f90e5aad0e.

--- train.py
+++ train.py
@@ -51,21 +51,22 @@ if __name__ == "__main__":
 
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-        lr.fit(train_x, train_y)
-
-        predicted_qualities = lr.predict(test_x)
-
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
-
-        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
-
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
+        for epoch in range(10):
+            lr.fit(train_x, train_y)
+
+            predicted_qualities = lr.predict(test_x)
+
+            (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+            print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+            print("  RMSE: %s" % rmse)
+            print("  MAE: %s" % mae)
+            print("  R2: %s" % r2)
+
+            mlflow.log_metric("rmse", rmse)
+            mlflow.log_metric("r2", r2)
+            mlflow.log_metric("mae", mae)
 
         mlflow.sklearn.log_model(lr, "model")
